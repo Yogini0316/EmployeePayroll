18bed617eec7fad51742d3f2ab2adae9d1b53978
Yogini0316 <yoginisonawane436@gmail.com>
2025-05-22 21:12:21 +0530

    Initial commit

---
+++ src/employee/payroll/system/db.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.payroll.system;
+
+/**
+ *
+ * @author BlackZora
+ */
+
+import java.sql.*;
+import javax.swing.*;
+public class db {
+    
+    
+    
+    Connection conn=null;
+    public static Connection java_db(){
+        
+        try{
+            Class.forName("org.sqlite.JDBC");
+           String url = "jdbc:sqlite:C:/Users/SWARA/Documents/NetBeansProjects/Payroll Management System Project in Java Netbeans Source Code/empnet.sqlite";
+Connection conn = DriverManager.getConnection(url);
+
+            //JOptionPane.showMessageDialog(null, "Connection to database is successful");
+      
+            return conn;
+           
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+    }
+}
